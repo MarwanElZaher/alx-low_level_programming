c9d45156413cea96af63a97224acdc555694991e
MarwanElZaher <marawanmzaher@gmail.com>
2023-02-17 02:04:55 +0200

    printing using pusts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - It is a program that prints a line given using puts function
+ * Return: 0 (means success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
