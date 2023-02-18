45950d3c3b0bfc7503cb3b9d9cc92b94014e1b87
MarwanElZaher <marawanmzaher@gmail.com>
2023-02-18 02:58:30 +0200

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - an app that print base 10 from zero
+ * Return: Alwayes 0 means(success)
+ */
+int main(void)
+
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+	{
+	printf("%d", a);
+	}
+printf("\n");
+return (0);
+}
