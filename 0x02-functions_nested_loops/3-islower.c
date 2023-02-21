0f36557fdc96d1bda696c54aed5467e3185eb1b6
MarwanElZaher <marawanmzaher@gmail.com>
2023-02-21 22:46:04 +0200

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - it checks for a lower case
+ * @c: checks that if the value is c print 1
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+	return (1);
+	}
+return (0);
+}
