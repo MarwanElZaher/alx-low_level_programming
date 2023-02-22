9673aa76833d1f9b2a8c96ca02687133bdb549d4
MarwanElZaher <marawanmzaher@gmail.com>
2023-02-22 02:09:09 +0200

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,32 +5,27 @@
  */
 void times_table(void)
 {
-int a, b, m;
-	for (a = 0 ; a < 10 ; a++)
-		{
-		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		for (b = 1 ; b < 10; b++)
+int num, mult, prod;
+
+	for (num = 0; num <= 9; num++)
 	{
-			m = ( a * b);
-		if ((m / 10) + '0')
-		{
-			_putchar((m / 10) + '0');
-		}
-		else
-		{
-			_putchar(' ');
-		}
-			_putchar((m % 10) + '0');
-		if (b < 9)
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
+
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-
-		}
-
 }
