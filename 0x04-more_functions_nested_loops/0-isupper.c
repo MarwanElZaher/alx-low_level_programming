3b2dbf887f4cbf5b7334be04d9aa9aea5ebdf4d2
MarwanElZaher <marawanmzaher@gmail.com>
2023-02-23 22:42:13 +0200

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - check for upper case
+ * @c: is the value that will contain the checked letter.
+ * Return: 1 if c is an upper case, 0 if lower case (otherwise).
+ */
+int _isupper(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+	return (1);
+	}
+return (0);
+}
