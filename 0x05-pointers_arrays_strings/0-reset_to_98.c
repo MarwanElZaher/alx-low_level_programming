479fa054906a9426376c04e5a5de3f6d9be22d4e
MarwanElZaher <marawanmzaher@gmail.com>
2023-02-27 15:36:50 +0200

    derefrencing pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * reset_to_98 - it is a function that take a pointer to an int as parameter
+ * and updates the value to 98
+ * @n: derefrencing pointer
+ * Return: nothing (void)
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+void reset_to_98(int *n);
+int _putchar(char);
+
+
+
+
+#endif
