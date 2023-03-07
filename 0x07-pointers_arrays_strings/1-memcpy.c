aa213ad4f2bb0a83e13771fe0eacee6018d09513
MarwanElZaher <marawanmzaher@gmail.com>
2023-03-07 02:50:16 +0200

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,12 +8,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-int c, n;
+unsigned int c;
 
 	for (c = 0 ; c < n ; c++)
 	{
-	dest[c] = src[r];
-	n--;
+	dest[c] = src[c];
 	}
 return (dest);
 }
