8345ab740f51a9d6376360012deb149ad65d34d4
MarwanElZaher <marawanmzaher@gmail.com>
2023-03-07 03:02:35 +0200

    5-strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,12 +7,11 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-char *a, *b;
 
 	for (; *haystack != '\0' ; haystack++)
 	{
-	*a = haystack;
-	*b = needle;
+	char *a = haystack;
+	char *b = needle;
 
 	while (*a == *b && *b != '\0')
 	{
