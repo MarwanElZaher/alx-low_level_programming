04fc6fb316fbdf0bf7b598b308eb02814c75481c
MarwanElZaher <marawanmzaher@gmail.com>
2023-03-10 21:39:48 +0200

    static library

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
- * _strcmp - it is a function used to compare two strings.
- * @s1: string 1 to be compared with string number 2.
- * @s2: string 2 to be compared with string number 1.
- * Return: always 0 (means success).
+ * _puts - prints string
+ * @str: input string
+ * Return: no return
  */
-int _strcmp(char *s1, char *s2)
-{
-int a;
 
-	for (a = 0 ; s1[a] != '\0' && s1[a] == s2[a] ;)
+void _puts(char *str)
+{
+	while (*str != '\0')
 	{
-	a++;
+		_putchar(*str);
+		str++;
 	}
-return (s1[a] - s2[a]);
+	_putchar('\n');
 }
