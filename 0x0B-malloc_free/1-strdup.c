fd3878be0aa9d7a5d41a80fa37120fbd1d25c783
MarwanElZaher <marawanmzaher@gmail.com>
2023-03-14 14:03:51 -0700

    fixed

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,7 +15,7 @@ int i;
 
 	if (str == 0)
 	return (NULL);
-		for (size = 0 ; str[size] != '\0' ; size++)
+		for (size = 0 ; str[size] ; size++)
 
 			pointer = (char *) malloc(sizeof(char) * size + 1);
 				if (pointer == 0)
