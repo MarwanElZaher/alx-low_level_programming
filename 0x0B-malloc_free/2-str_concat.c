c4e5a18ac51b48fc03e8b1ae1fe86f2e9293673f
MarwanElZaher <marawanmzaher@gmail.com>
2023-03-14 15:46:55 -0700

    error solved

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,10 +17,10 @@ int i, j;
 		{
 		s1 = "";
 		}
-		else if (s2 == NULL)
-			{
-			s2 = "";
-			}
+	if (s2 == NULL)
+	{
+	s2 = "";
+	}
 	for (lengths1 = 0 ; s1[lengths1] != '\0' ;)
 	{
 		lengths1++;
