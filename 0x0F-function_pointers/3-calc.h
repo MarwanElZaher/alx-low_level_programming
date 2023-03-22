3ac6ce1d629cc8fb766a9d798b0cfbaada4d2f14
Marwan ElZaher <marawanmzaher@hotmail.com>
2023-03-23 01:21:52 +0200

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,9 @@
+#ifndef CALC_H
+#define CALC_H
 
-#ifndef _HEADER_
-#define _HEADER_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct op - Struct op
@@ -20,4 +23,5 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+
 #endif
